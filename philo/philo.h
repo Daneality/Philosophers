a2523e5f8ae1ab5b232a6c7b21add6d29a7541ee
dsas <dsas@student.42wolfsburg.de>
2023-03-20 11:55:21 +0100

    some structures + utils

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: dsas <dsas@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:25:28 by dsas              #+#    #+#             */
-/*   Updated: 2023/03/20 10:26:52 by dsas             ###   ########.fr       */
+/*   Updated: 2023/03/20 11:55:01 by dsas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,32 @@
 
 uint64_t	ft_get_time(void);
 void		ft_usleep(uint64_t wait_time);
+int			ft_atoi(const char *nptr);
+
+typedef struct s_gdata
+{
+	int				ready;
+	int				tte;
+	int				ttd;
+	int				tts;
+	int				tme;
+	int				over;
+	int				nop;
+	int				check_tme;
+	pthread_mutex_t	*death;
+	pthread_mutex_t	*fork;
+} t_gdata;
+
+typedef struct s_philo
+{
+	int				id;
+	int				iter;
+	long int		start;
+	long int		meal;
+	pthread_t		phils;
+	pthread_mutex_t	*forkl;
+	pthread_mutex_t	*forkr;
+	t_gdata			*data;
+} t_philo;
 
 #endif
--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: dsas <dsas@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:25:17 by dsas              #+#    #+#             */
-/*   Updated: 2023/03/20 10:25:24 by dsas             ###   ########.fr       */
+/*   Updated: 2023/03/20 10:43:05 by dsas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,31 @@ void	ft_usleep(uint64_t wait_time)
 	time = ft_get_time();
 	while (ft_get_time() - time < wait_time)
 		usleep(100);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	i;
+	int	sum;
+
+	sum = 0;
+	sign = 1;
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		sum *= 10;
+		sum += nptr[i] - '0';
+		i++;
+	}
+	sum *= sign;
+	return (sum);
 }
